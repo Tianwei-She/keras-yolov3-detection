4df0d56d21e374dcd064c6cba40b2807146761c7
Tianwei She <shetw@node08-ccncluster.stanford.edu>
2020-05-26 23:18:39 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# keras-yolov3-detection
+# YOLOv3-detection
+
+This repo implements object detection on one single image or one video, using a pretrained YOLOv3 model. The output is saved in an image/video file with detected bounding boxes plotted.
+
+
+```
+wget https://pjreddie.com/media/files/yolov3.weights -P PATH/TO/WEIGHTS_FOLDER
+python convert.py data/yolov3.cfg PATH/TO/WEIGHTS_FOLDER/yolov3.weights PATH/TO/WEIGHTS_FOLDER/yolov3.h5
+# Image
+python detect.py --model_path PATH/TO/WEIGHTS_FOLDER/yolov3.h5 --input_path PATH/TO/INPUT/IMAGE --output_path PATH/TO/OUTPUT/IMAGE
+# Video
+python detect.py --video --model_path PATH/TO/WEIGHTS_FOLDER/yolov3.h5 --input_path PATH/TO/INPUT/VIDEO --output_path PATH/TO/OUTPUT/VIDEO
+```
---
+++ detect.py
@@ -0,0 +1,366 @@
+import argparse
+import tensorflow as tf
+import numpy as np
+import cv2
+from copy import deepcopy
+from PIL import Image, ImageDraw, ImageFont
+import colorsys
+
+
+labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", \
+            "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", \
+            "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", \
+            "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", \
+            "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", \
+            "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", \
+            "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", \
+            "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", \
+            "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", \
+            "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]  
+            
+anchors = [[[116,90], [156,198], [373,326]], [[30,61], [62,45], [59,119]], [[10,13], [16,30], [33,23]]]
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+            description='The script to run object detection on one image or one video')
+    parser.add_argument(
+            '--model_path',
+            type=str, action='store',
+            help='Path to the saved model (.h5 file)')
+    parser.add_argument(
+            '--input_path',
+            type=str, action='store',
+            help='Path to the input file that contains the image/video to apply YOLO on')
+    parser.add_argument(
+            '--output_path',
+            type=str, action='store',
+            help='Path to the output file that saves the detection result')
+    parser.add_argument(
+            '--obj_thresh',
+            type=float, action='store',
+            default=0.4,
+            help='The objectness threshold')
+    parser.add_argument(
+            '--nms_thresh',
+            type=float, action='store',
+            default=0.45,
+            help='The IoU threshold for non max suppression')
+    parser.add_argument(
+            '--video', action='store_true',
+            help='Whether the input is a video')
+    return parser
+
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, objness = None, classes = None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        
+        self.objness = objness
+        self.classes = classes
+
+        self.label = -1
+        self.score = -1
+
+    def get_label(self):
+        if self.label == -1:
+            self.label = np.argmax(self.classes)
+        
+        return self.label
+    
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.get_label()]
+            
+        return self.score
+
+def _interval_overlap(interval_a, interval_b):
+    x1, x2 = interval_a
+    x3, x4 = interval_b
+
+    if x3 < x1:
+        if x4 < x1:
+            return 0
+        else:
+            return min(x2,x4) - x1
+    else:
+        if x2 < x3:
+             return 0
+        else:
+            return min(x2,x4) - x3          
+
+def _sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def bbox_iou(box1, box2):
+    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
+    
+    intersect = intersect_w * intersect_h
+
+    w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
+    w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
+    
+    union = w1*h1 + w2*h2 - intersect
+    
+    return float(intersect) / union
+
+def preprocess_input(image_pil, net_h, net_w):
+    image = np.asarray(image_pil)
+    new_h, new_w, _ = image.shape
+
+    # determine the new size of the image
+    if (float(net_w)/new_w) < (float(net_h)/new_h):
+        new_h = (new_h * net_w)/new_w
+        new_w = net_w
+    else:
+        new_w = (new_w * net_h)/new_h
+        new_h = net_h
+
+    # resize the image to the new size
+    #resized = cv2.resize(image[:,:,::-1]/255., (int(new_w), int(new_h)))
+    resized = cv2.resize(image/255., (int(new_w), int(new_h)))
+
+    # embed the image into the standard letter box
+    new_image = np.ones((net_h, net_w, 3)) * 0.5
+    new_image[int((net_h-new_h)//2):int((net_h+new_h)//2), int((net_w-new_w)//2):int((net_w+new_w)//2), :] = resized
+    new_image = np.expand_dims(new_image, 0)
+
+    return new_image
+
+
+def decode_netout(netout_, obj_thresh, anchors_, image_h, image_w, net_h, net_w):
+    netout_all = deepcopy(netout_)
+    boxes_all = []
+    for i in range(len(netout_all)):
+      netout = netout_all[i][0]
+      anchors = anchors_[i]
+
+      grid_h, grid_w = netout.shape[:2]
+      nb_box = 3
+      netout = netout.reshape((grid_h, grid_w, nb_box, -1))
+      nb_class = netout.shape[-1] - 5
+
+      boxes = []
+
+      netout[..., :2]  = _sigmoid(netout[..., :2])
+      netout[..., 4:]  = _sigmoid(netout[..., 4:])
+      netout[..., 5:]  = netout[..., 4][..., np.newaxis] * netout[..., 5:]
+      netout[..., 5:] *= netout[..., 5:] > obj_thresh
+
+      for i in range(grid_h*grid_w):
+          row = i // grid_w
+          col = i % grid_w
+          
+          for b in range(nb_box):
+              # 4th element is objectness score
+              objectness = netout[row][col][b][4]
+              #objectness = netout[..., :4]
+              # last elements are class probabilities
+              classes = netout[row][col][b][5:]
+              
+              if((classes <= obj_thresh).all()): continue
+              
+              # first 4 elements are x, y, w, and h
+              x, y, w, h = netout[row][col][b][:4]
+
+              x = (col + x) / grid_w # center position, unit: image width
+              y = (row + y) / grid_h # center position, unit: image height
+              w = anchors[b][0] * np.exp(w) / net_w # unit: image width
+              h = anchors[b][1] * np.exp(h) / net_h # unit: image height  
+            
+              box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, objectness, classes)
+              #box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, None, classes)
+
+              boxes.append(box)
+
+      boxes_all += boxes
+
+    # Correct boxes
+    boxes_all = correct_yolo_boxes(boxes_all, image_h, image_w, net_h, net_w)
+    
+    return boxes_all
+
+def correct_yolo_boxes(boxes_, image_h, image_w, net_h, net_w):
+    boxes = deepcopy(boxes_)
+    if (float(net_w)/image_w) < (float(net_h)/image_h):
+        new_w = net_w
+        new_h = (image_h*net_w)/image_w
+    else:
+        new_h = net_w
+        new_w = (image_w*net_h)/image_h
+        
+    for i in range(len(boxes)):
+        x_offset, x_scale = (net_w - new_w)/2./net_w, float(new_w)/net_w
+        y_offset, y_scale = (net_h - new_h)/2./net_h, float(new_h)/net_h
+        
+        boxes[i].xmin = int((boxes[i].xmin - x_offset) / x_scale * image_w)
+        boxes[i].xmax = int((boxes[i].xmax - x_offset) / x_scale * image_w)
+        boxes[i].ymin = int((boxes[i].ymin - y_offset) / y_scale * image_h)
+        boxes[i].ymax = int((boxes[i].ymax - y_offset) / y_scale * image_h)
+    return boxes
+        
+def do_nms(boxes_, nms_thresh, obj_thresh):
+    boxes = deepcopy(boxes_)
+    if len(boxes) > 0:
+        num_class = len(boxes[0].classes)
+    else:
+        return
+        
+    for c in range(num_class):
+        sorted_indices = np.argsort([-box.classes[c] for box in boxes])
+
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+
+            if boxes[index_i].classes[c] == 0: continue
+
+            for j in range(i+1, len(sorted_indices)):
+                index_j = sorted_indices[j]
+
+                if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_thresh:
+                    boxes[index_j].classes[c] = 0
+
+    new_boxes = []
+    for box in boxes:
+        label = -1
+        
+        for i in range(num_class):
+            if box.classes[i] > obj_thresh:
+                label = i
+                # print("{}: {}, ({}, {})".format(labels[i], box.classes[i]*100, box.xmin, box.ymin))
+                box.label = label
+                box.score = box.classes[i]
+                new_boxes.append(box)    
+
+    return new_boxes
+
+
+def draw_boxes(image_, boxes, labels):
+    image = image_.copy()
+    image_w, image_h = image.size
+    font = ImageFont.truetype(font='/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf',
+                    size=np.floor(3e-2 * image_h + 0.5).astype('int32'))
+    thickness = (image_w + image_h) // 300
+
+    # Generate colors for drawing bounding boxes.
+    hsv_tuples = [(x / len(labels), 1., 1.)
+                  for x in range(len(labels))]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(
+        map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    np.random.seed(10101)  # Fixed seed for consistent colors across runs.
+    np.random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
+    np.random.seed(None)  # Reset seed to default.
+
+    for i, box in reversed(list(enumerate(boxes))):
+        c = box.get_label()
+        predicted_class = labels[c]
+        score = box.get_score()
+        top, left, bottom, right = box.ymin, box.xmin, box.ymax, box.xmax
+
+        label = '{} {:.2f}'.format(predicted_class, score)
+        draw = ImageDraw.Draw(image)
+        label_size = draw.textsize(label, font)
+        #label_size = draw.textsize(label)
+
+        top = max(0, np.floor(top + 0.5).astype('int32'))
+        left = max(0, np.floor(left + 0.5).astype('int32'))
+        bottom = min(image_h, np.floor(bottom + 0.5).astype('int32'))
+        right = min(image_w, np.floor(right + 0.5).astype('int32'))
+        print(label, (left, top), (right, bottom))
+
+        if top - label_size[1] >= 0:
+            text_origin = np.array([left, top - label_size[1]])
+        else:
+            text_origin = np.array([left, top + 1])
+
+        # My kingdom for a good redistributable image drawing library.
+        for i in range(thickness):
+            draw.rectangle(
+                [left + i, top + i, right - i, bottom - i],
+                outline=colors[c])
+        draw.rectangle(
+            [tuple(text_origin), tuple(text_origin + label_size)],
+            fill=colors[c])
+        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        #draw.text(text_origin, label, fill=(0, 0, 0))
+        del draw
+    return image  
+
+
+def detect_image(image_pil, darknet, obj_thresh=0.4, nms_thresh=0.45, net_h=416, net_w=416, anchors=anchors, labels=labels):
+
+    # Preprocessing
+    image_w, image_h = image_pil.size
+    new_image = preprocess_input(image_pil, net_h, net_w)
+
+    # DarkNet
+    yolo_outputs = darknet.predict(new_image)
+
+    # Decode the output of the network
+    boxes = decode_netout(yolo_outputs, obj_thresh, anchors, image_h, image_w, net_h, net_w)
+
+    # Suppress non-maximal boxes
+    boxes = do_nms(boxes, nms_thresh, obj_thresh)
+
+    # Draw bounding boxes on the image using labels
+    image_detect = draw_boxes(image_pil, boxes, labels) 
+
+    return image_detect
+
+import cv2
+
+def detect_video(video_path, output_path, darknet, obj_thresh=0.4, nms_thresh=0.45, net_h=416, net_w=416, anchors=anchors, labels=labels):
+    vid = cv2.VideoCapture(video_path)
+    if not vid.isOpened():
+        raise IOError("Couldn't open webcam or video")
+    
+    video_FourCC    = int(vid.get(cv2.CAP_PROP_FOURCC))
+    video_FourCC = cv2.VideoWriter_fourcc(*'mp4v')
+    video_fps       = vid.get(cv2.CAP_PROP_FPS)
+    video_size      = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                        int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    
+    out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
+
+    num_frame = 0
+    while vid.isOpened():
+      ret, frame = vid.read()
+      num_frame += 1
+      print("=== Frame {} ===".format(num_frame))
+      if ret:
+          frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+          frame = Image.fromarray(frame)
+
+          result = detect_image(frame, darknet, obj_thresh=obj_thresh, nms_thresh=nms_thresh)
+          
+          new_frame = np.asarray(result)
+          new_frame = cv2.cvtColor(new_frame, cv2.COLOR_RGB2BGR)
+
+          out.write(new_frame)
+      else:
+          break
+
+    vid.release()
+    out.release()
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    # Load model
+    darknet = tf.keras.models.load_model(args.model_path)
+
+    if args.video:
+        detect_video(args.input_path, args.output_path, darknet, obj_thresh=args.obj_thresh, nms_thresh=args.nms_thresh)        
+    else:
+        image_pil = Image.open(args.input_path)
+        image_detect = detect_image(image_pil, darknet, obj_thresh=args.obj_thresh, nms_thresh=args.nms_thresh)
+        image_detect.save(args.output_path)
+
+
+if __name__ == '__main__':
+    main()
